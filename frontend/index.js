313bfebcf86d3ff6d3b0592f00df215040f1a8b2
Yevgeniy <anigeka@gmail.com>
2021-03-10 19:58:51 -0500

    add basic overview charts

--- frontend/index.js
+++ frontend/index.js
@@ -10,6 +10,8 @@ const nutritionLink = document.querySelector('.nutrition-link');
 const exerciseLink = document.querySelector('.exercise-link');
 const waterLink = document.querySelector('.water-link');
 
+
+
 sectionNavs.forEach(section => {
     section.addEventListener('mouseover', blurNavbar);
     section.addEventListener('mouseout', unblurNavbar);
@@ -31,3 +33,90 @@ function unblurNavbar(e) {
             allSections[i].classList.remove('blurred');
     }
 }
+
+var trend = document.getElementById('trendChart');
+var myLineChart = new Chart(trend, {
+    type: 'line',
+    data: {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        datasets: [
+            {
+                label: "Prime and Fibonacci",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+            }
+        ]
+    },
+    options: {
+        responsive: false,
+        maintainAspectRatio: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById('nutritionDaily');
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Nutrition',
+            data: [12, 19, 3, 5, 100, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+        maintainAspectRatio: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+ctx.addEventListener('click', function (e) {
+    const activePoints = myChart.getElementsAtEventForMode(e, 'point', myChart.options);
+    const firstPoint = activePoints[0];
+    const label = myChart.data.labels[firstPoint._index];
+    const value = myChart.data.datasets[firstPoint._datasetIndex].data[firstPoint._index];
+    alert(label + ": " + value);
+});
+
+trend.addEventListener('click', function (evt) {
+    var firstPoint = myLineChart.getElementAtEvent(evt)[0];
+    console.log(firstPoint);
+    console.log(myLineChart.data.labels)
+    myLineChart.data.labels.pop();
+    myLineChart.update();
+});
