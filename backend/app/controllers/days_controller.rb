8c837d7a75d900fe993a2e6debe6674415eed562
Yevgeniy <anigeka@gmail.com>
2021-03-14 18:09:25 -0400

    add days model

---
+++ backend/app/controllers/days_controller.rb
@@ -0,0 +1,17 @@
+class DaysController < ApplicationController
+    def index
+        render json: { user: User.first }
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_011122) do
+ActiveRecord::Schema.define(version: 2021_03_13_193419) do
+
+  create_table "days", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "weight"
+    t.integer "calories"
+    t.integer "protein"
+    t.integer "carbs"
+    t.integer "fat"
+    t.integer "water"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_days_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -20,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_03_11_011122) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "days", "users"
 end
