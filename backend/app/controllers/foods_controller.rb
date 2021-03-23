c11ef8c9be67adc468e8e8e2825c1a9ae553ffc6
Yevgeniy <anigeka@gmail.com>
2021-03-22 22:11:45 -0400

    add searchbar result endpoint

--- backend/app/controllers/foods_controller.rb
+++ backend/app/controllers/foods_controller.rb
@@ -1,7 +1,8 @@
 class FoodsController < ApplicationController
+    require 'httparty'
     def index
-        day = Time.parse("#{params[:year]}-#{params[:month]}-#{params[:day]}")
-        render json: { user: User.where(created_at: day.midnight..day.end_of_day)}
+        # day = Time.parse("#{params[:year]}-#{params[:month]}-#{params[:day]}")
+        # render json: { user: User.where(created_at: day.midnight..day.end_of_day)}
     end
 
     def create 
@@ -17,11 +18,12 @@ class FoodsController < ApplicationController
     end
 
     def searchbar
-
+        results = searchbar_info(query: 'chicken', size: 5)
+        render json: { results: results }
     end
 
 private 
     def searchbar_info(query:, size:)
-        
+        HTTParty.get("https://trackapi.nutritionix.com/v2/search/instant?query=#{query}", headers: {"x-app-id": ENV['NUTRITIONIX_ID'], "x-app-key": ENV['NUTRITIONIX_KEY'], "x-remote-user-id": '0'})
     end
 end
